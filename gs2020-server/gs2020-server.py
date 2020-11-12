b9fe94e4fb55ddc0409c3fe3cda6dee631aae7cb
James Atkinson <james@theatkinsons.ca>
2020-11-11 21:28:09 -0700

    alpha-0.0.1

--- gs2020-server/gs2020-server.py
+++ gs2020-server/gs2020-server.py
@@ -5,7 +5,13 @@ from time import sleep
 import random
 import sys
 
-app = Flask(__name__)
+if getattr(sys, 'frozen', False):
+    template_folder = os.path.join(sys._MEIPASS, 'templates')
+    static_folder = os.path.join(sys._MEIPASS, 'static')
+    app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+else:
+    app = Flask(__name__)
+
 CORS(app)
 
 import logging
